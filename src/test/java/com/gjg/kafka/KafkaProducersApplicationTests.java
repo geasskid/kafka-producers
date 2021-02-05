6e255e652b7f69d268e0db4f71395cbfc04951fe
geasskid <geasskid@163.com>
2021-02-05 11:53:43 +0800

    创建kafka生产者项目

---
+++ src/test/java/com/gjg/kafka/KafkaProducersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjg.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
